2b51efb2d664649ab93135035c3be6da8a8ade85
rohansinghwow <rohansinghwow@gmail.com>
2021-12-26 12:56:13 +0530

    Adding readme and screenshot

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Main(){
     return (
         <h1  className="text-center pt-5 text-3xl font-semibold underline">
-          Accrodian in React
+          Accorodian in React
       </h1>
     )
 }
